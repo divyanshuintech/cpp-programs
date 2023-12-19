5e231d33e077901869f4648c2d0a983e02bc1167
divyanshuintech <iitiandivyanshu02582@gmail.com>
2023-12-19 11:56:18 +0530

    first commit

---
+++ binaryTree.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *buildTree(Node *root)
+{
+    int data;
+    cin >> data;
+    root = new Node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter data for inserting in left of " << data << endl;
+    root->left = buildTree(root->left);
+    cout << "Enter data for inserting in right of " << data << endl;
+    root->right = buildTree(root->right);
+}
+
+void levelOrder(Node *root)
+{
+    
+}
+
+void inorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void preorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    // base case
+    if (root == NULL)
+    {
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = buildTree(root);
+    // Data for making tree : 1 2 4 -1 -1 5 -1 -1 3 6 -1 -1 7 -1 -1
+    cout << endl
+         << "Inorder Traversal below : " << endl;
+    inorder(root);
+    cout << endl
+         << "Preorder Traversal below : " << endl;
+    preorder(root);
+    cout << endl
+         << "Postorder Traversal below : " << endl;
+    postorder(root);
+    return 0;
+}
